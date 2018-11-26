90504e628b184db229333246a1003c61199aff63
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-11-26 15:58:34 -0800

    further study done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, render_template
+from flask import Flask, request, session, render_template, redirect
 # from flask_debugtoolbar import DebugToolbarExtension
 from surveys import surveys
 
@@ -7,28 +7,72 @@ app.config['SECRET_KEY'] = "blah"
 
 # debug = DebugToolbarExtension(app)
 curr_survey = surveys['satisfaction']
+comments = []
 
 
 @app.route('/')
-def start_survey():
+def select_survey():
+    return render_template('select.html', surveys=surveys)
+
+
+@app.route('/', methods=["POST"])
+def redirect_selected_survey():
+    selected = request.form['selected_survey']
+    global curr_survey
+    curr_survey = surveys[selected]
+    return redirect(f'/{selected}')
+
+
+@app.route('/<selected_survey>')
+def start_survey(selected_survey):
+    '''Generates the starter page for starting a survey'''
+
     session['survey'] = []
     return render_template(
         'survey.html',
+        survey=selected_survey,
         title=curr_survey.title,
         instructions=curr_survey.instructions)
 
 
-@app.route('/questions/<int:number>', methods=["POST"])
-def question_page(number):
+@app.route('/<selected_survey>/questions/<int:number>', methods=["POST"])
+def question_page(selected_survey, number):
+    '''Loops through each question and updates the survey answers in the session list.'''
+
     if not number == 0:
         survey = session['survey']
         survey.append(request.form['answer'])
         session['survey'] = survey
+        comment = request.form.get('comment', None)
+        print(f"""
+        
+        {comment}
+        
+        
+        """)
+        comments.append(comment)
+        print(f"""
+        
+        {comments}
+        
+        
+        """)
 
     if number < len(curr_survey.questions):
         return render_template(
             'questions.html',
+            survey=selected_survey,
+            comments=comments,
             question=curr_survey.questions[number],
             number=number + 1)
     else:
-        return render_template('thanks.html')
+        return redirect('/thanks')
+
+
+@app.route('/thanks')
+def thanks():
+    return render_template(
+        'thanks.html',
+        comments=comments,
+        enum=enumerate(session['survey']),
+        question=curr_survey.questions)
